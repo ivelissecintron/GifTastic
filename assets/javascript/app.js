a649f9c4b533e42035b1dfbde1e66a18792c00d3
Ivelisse Cintron <33500791+ivelissecintron@users.noreply.github.com>
2018-04-28 00:50:19 -0500

    updated html, css, js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,93 @@
-// array of office members
-var officeArray = ["michael scott", "jim halpert", "dwight schrute", "pam beesly", "toby flenderson", "kevin malone", "meredith palmer", "stanley hudson", "oscar martinez", "creed bratton"]; 
+// array of topics
+var topics = ["Michael Scott", "Jim Halpert", "Dwight Schrute", "Pam Beesly", "Toby Flenderson", "Kevin Malone", "Meredith Palmer", "Stanley Hudson", "Oscar Martinez", "Creed Bratton"]; 
 
-// creates buttons for each item in the officeArray
-function renderButtons() {
-    //deletes buttons prior to adding new buttons to avoid repeat buttons
-    $("#officeButtons").empty();    
-    //looping through the array of office members
-    for (var i = 0; i < officeArray.length; i++) {
-        //generates buttons for each office member in the array
-        var a = $("<button>")
-        //adding a class of officemember to button
-        a.addClass("officemember");
-        //adding a data-attribute
-        a.attr("data-name", officeArray[i]);
-        //provides button text
-        a.text(officeArray[i]);
-        //adding the button to the buttons div
-        $("#officeButtons").append(a);
+// function to displayOfficeGifs  
+function displayOfficeGifs() {
+    $("#theoffice").empty();
+    
+	var theoffice = $(this).attr("data-name");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + theoffice + "&limit=10&api_key=MSvi3HgAogpJ3zByMDGwTkYfBHA2aw7a";
+    
+    //creating an ajax call to hit the giphy api
+	$.ajax({
+	  url: queryURL,
+	  method: "GET"
+	}).done(function(response) {
+	  console.log(response);
+      
+	  var results = response.data;
+          //for loop thru the topics array    
+          for (var i = 0; i < results.length; i++) {
+            //adding class for gifs   
+            var gifDiv = $("<div class='item'>");
+            // grabbing the rating 
+            var rating = results[i].rating;
+            //creating an element to have the rating displayed
+            var p = $("<p>").text("Rating: " + rating);
+            //creating an element to hold the office images 
+            var officeImage = $("<img>");
+            officeImage.attr("src", results[i].images.fixed_height_still.url).attr("data-still", results[i].images.fixed_height_still.url).attr("data-animate", results[i].images.fixed_height.url).attr("data-state","still").attr("class","gif img-rounded img-responsive");
+            //prepending the image
+            gifDiv.prepend(officeImage);
+            gifDiv.prepend(p);
+
+            $("#theoffice").prepend(gifDiv);
+          }	  
+	});
+
+}
+
+// Function for rendering buttons
+  function renderButtons() {
+
+    // to avoid repeat buttons
+    $("#officebuttons").empty();
+
+    // Looping through the array of topics
+    for (var i = 0; i < topics.length; i++) {
+
+      var a = $("<button>");
+      // Adding a class of officeBtn 
+      a.addClass("officeBtn btn btn-default");
+      // Adding a data-attribute
+      a.attr("data-name", topics[i]);
+      // Providing the initial button text
+      a.text(topics[i]);
+      // Adding the button to the officebuttons div
+      $("#officebuttons").append(a);
     }
-}
+  }
 
-//function handles events when one button is clicked
-$("#addOffice").on("click", function(){
-    
-    //grabs the input from the text
-    var newOfficeMember = $("#office-input").val().trim();
-    //adding the text from the textbox and adding to my array
-    officeArray.push(newOfficeMember);
-    //calling renderButtons which handles the processing of my array
-    renderButtons();
-})
+  //function to render buttons
+  renderButtons();
+  //on click event 
+  $(document).on("click", ".gif", function(){
+        //running debugger
+     	console.log("Hello!");
+        
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is
+      if (state==="still"){
+        $(this).attr("src", $(this).attr("data-animate"))
+        .attr("data-state","animate");  
+      }
+      // Else set src to the data-still value
+      else{
+        $(this).attr("src", $(this).attr("data-still"))
+        .attr("data-state","still"); 
+      }
+    });
 
+      // This function handles events when the add office button is clicked
+      $("#addoffice").on("click", function(event) {
+        event.preventDefault();
+        // grabs the input from the textbox
+        var  officeadd = $("#office-input").val().trim();
+          // pushing new office add from the textbox to our array  
+          topics.push(officeadd);
+          // Calling renderButtons to add new button
+          renderButtons();
+      });
 
-//function for displaying office members
-function displayOffice() {
-    
-    var theoffice = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + theoffice +"&api_key=MSvi3HgAogpJ3zByMDGwTkYfBHA2aw7a&limit=10";
-
-        $.ajax({
-        url: queryURL,
-        method: "GET"
-        }).then(function(response){
-        console.log(response.data);
-        });
-}
-
-//function for displaying theoffice
-$(document).on("click", ".theoffice", displayOffice);
-//calls the renderButtons function
-renderButtons();
+      // Adding a click event listener to all elements with a class of "officeBtn"
+      $(document).on("click", ".officeBtn", displayOfficeGifs);
